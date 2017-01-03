e174f43a5aeabfe28208c44e285e3f359d3b9814
Stanislav Yudin <stan@endlessinsomnia.com>
2017-01-03 17:33:08 +1100

    update example with python on_load

--- examples/generic/src/example.py
+++ examples/generic/src/example.py
@@ -1,18 +1,20 @@
 import kore
+import datetime
 import urllib.parse
 
 def on_load(state):
-    pass
+	print('python -> on_load: %s' % ('loading' if state == kore.MODULE_LOAD else 'unloading'))
+	return kore.RESULT_OK
 
 def on_request(req):
-	print("on_request: %s" % repr(req))
-	print("agent: %s" % req.agent)
-	print("host: %s" % req.host)
-	print("path: %s" % req.path)
-	print("body: %s" % req.body)
+	print('python -> on_request: %s' % repr(req))
+	print('\tagent: %s' % req.agent)
+	print('\thost: %s' % req.host)
+	print('\tpath: %s' % req.path)
+	print('\tbody: %s' % req.body)
 
 	kore.http_response_header(req, 'Content-Type', 'text/plain')
 
-	body = "Hello World, %s" % urllib.parse.quote(req.agent)
+	body = 'Hello World, time is %s' % datetime.datetime.now().isoformat()
 	kore.http_response(req, 200, body.encode('utf-8'))
     

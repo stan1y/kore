07692569b6d538c385aca6d14784acecd2a2d3ac
Stanislav Yudin <stan@endlessinsomnia.com>
2017-01-05 17:57:12 +1100

    add file upload example in python

--- examples/generic/src/example.py
+++ examples/generic/src/example.py
@@ -6,15 +6,84 @@ def on_load(state):
 	print('python -> on_load: %s' % ('loading' if state == kore.MODULE_LOAD else 'unloading'))
 	return kore.RESULT_OK
 
-def on_request(req):
+def method_name(mth):
+	if mth == kore.METHOD_GET:
+		return 'GET'
+	if mth == kore.METHOD_POST:
+		return 'POST'
+	if mth == kore.METHOD_PUT:
+		return 'PUT'
+	if mth == kore.METHOD_DELETE:
+		return 'DELETE'
+	if mth == kore.METHOD_HEAD:
+		return 'HEAD'
+	return 'UNKNOWN'
+
+def hello_world(req):
 	print('python -> on_request: %s' % repr(req))
 	print('\tagent: %s' % req.agent)
+	print('\tmethod: %s' % method_name(req.method))
 	print('\thost: %s' % req.host)
 	print('\tpath: %s' % req.path)
 	print('\tbody: %s' % req.body)
 
-	kore.http_response_header(req, 'Content-Type', 'text/plain')
+	req.response_header('Content-Type', 'text/plain')
 
 	body = 'Hello World, time is %s' % datetime.datetime.now().isoformat()
-	kore.http_response(req, 200, body.encode('utf-8'))
-    
+	req.response(200, body.encode('utf-8'))
+	return kore.RESULT_OK
+
+
+upload_html = '''
+<!DOCTYPE>
+<html>
+<head>
+	<link rel="stylesheet" href="/css/style.css" type="text/css">
+	<title>Kore upload test</title>
+</head>
+
+<body style="overflow: auto">
+
+<div class="content">
+	<form method="POST" enctype="multipart/form-data">
+		<input type="file" name="file">
+		<input type="submit" value="upload">
+	</form>
+
+	<p style="font-size: 12px; font-weight: normal">{upload}</p>
+</div>
+
+</body>
+</html>
+'''
+
+def handle_file_upload(req):
+	print('handle_file_upload')
+	req.response_header('Content-Type', 'text/html')
+
+	if req.method == kore.METHOD_GET:
+		print('->rendering upload form')
+		req.response(200,
+			upload_html.format(upload='').encode('utf-8'))
+
+	if req.method == kore.METHOD_POST:
+		if req.bodyfd != -1:
+			print("file is on disk")
+
+		req.populate_multipart_form()
+		file = req.get_file("file")
+		if file:
+			print("->got file!")
+			print("  filename: %s" % file.filename)
+			print("  length: %s" % file.length)
+			print("  position: %s" % file.position)
+			print("  offset: %s" % file.offset)
+
+			answer = upload_html.format(upload='%s is %d bytes' % 
+					(file.filename, file.length))
+
+			req.response(200, answer.encode('utf-8'))
+		else:
+			req.response(400, "No file received".encode('utf-8'))
+
+	return kore.RESULT_OK
